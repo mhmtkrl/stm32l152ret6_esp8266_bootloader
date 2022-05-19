750511ee038f394bf20c4961ad204a12ac2d7f76
mhmtkrl <koral.mehmet45@gmail.com>
2022-05-19 17:06:34 +0300

    first release

---
+++ RTE/_Source_Codes/Pre_Include_Global.h
@@ -0,0 +1,18 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Wifi_Bootloader' 
+ * Target:  'Source Codes' 
+ */
+
+#ifndef PRE_INCLUDE_GLOBAL_H
+#define PRE_INCLUDE_GLOBAL_H
+
+/* Keil.Standalone::Device:Startup:1.10.3 */
+#define USE_HAL_DRIVER
+          #define USE_FULL_LL_DRIVER
+
+
+#endif /* PRE_INCLUDE_GLOBAL_H */
