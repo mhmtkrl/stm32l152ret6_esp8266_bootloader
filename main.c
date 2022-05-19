db6146a45545db1db1523a8abba80817fdfa287a
mhmtkrl <koral.mehmet45@gmail.com>
2022-05-19 18:41:35 +0300

    UART was developed for debugging purpose

--- main.c
+++ main.c
@@ -1,10 +1,16 @@
 #include "stm32l1xx.h"                  // Device header
 #include "gpio_config.h"
 #include "sysTick_timer_config.h"
+#include "uart_config.h"
+
+uint8_t txt[] = "FOTA over WiFi!\r\n";
 
 int main() {
 	InitSysTickTimerInMiliseconds(1, (uint32_t)CLOCK_FREQ);
+	InitUARTforDebug();
 	InitUserLED();
+	
+	UARTDebugSend(txt);
 	
 	while(1) {
 		OnUserLED();
